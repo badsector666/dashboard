19dbd44b4f0c032685acf7eeb954b693c689297e
yoratoni <71235233+yoratoni@users.noreply.github.com>
2023-06-21 08:00:49 +0200

    Added BlueprintJS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
+import { Button, Icon } from "@blueprintjs/core";
+
+
 export default function Home() {
     return (
         <div>
             <h1>Dashboard</h1>
+
+            <Button icon="refresh" intent="danger" text="Reset" />
+            <Button icon="user" rightIcon="caret-down" text="Profile settings" />
+            <Button rightIcon="arrow-right" intent="success" text="Next step" />
+            {/* <Icon> children as inline text elements */}
+            <Button>
+                <Icon icon="document" /> Upload... <Icon icon="small-cross" />
+            </Button>
         </div>
     );
 }
